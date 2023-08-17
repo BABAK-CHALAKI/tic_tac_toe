78c2daa87366b78ebe29747c1e91a22617d99390
mr.chalaki <129165551+BABAK-CHALAKI@users.noreply.github.com>
2023-08-16 22:50:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ let restart = document.getElementById("restart")
 let scores_x = document.getElementById("scores_x")
 let scores_o = document.getElementById("scores_o")
 let draw = document.getElementById("scores_draw")
+let xbtn = document.getElementById("xbtn")
+let obtn = document.getElementById("obtn")
+let rel = document.getElementById("rel")
+let h1 = document.getElementById("h1")
+let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
+let b3 = document.getElementById("b3")
+let b4 = document.getElementById("b4")
+let b5 = document.getElementById("b5")
+let b6 = document.getElementById("b6")
+let b7 = document.getElementById("b7")
+let b8 = document.getElementById("b8")
+let b9 = document.getElementById("b9")
 let scores_x_counter = 0;
 let scores_o_counter = 0;
 let draw_counter = 0;
@@ -16,7 +29,19 @@ let  flags = [[null, null, null],
               [null, null, null],
               [null, null, null]];
 
- let ply = "x";
+  xbtn.onclick = function(){
+   ply = "x"
+   xbtn.style.display = "none"
+   obtn.style.display = "none"
+   h1.style.display = "none"
+ }
+
+ obtn.onclick = function(){
+   ply = "o"
+   xbtn.style.display = "none"
+   obtn.style.display = "none"
+   h1.style.display = "none"
+ }
 
  
 
@@ -112,8 +137,38 @@ function check_game(){
                else if(flags[x][y] == null){
                   draw.innerHTML = draw_counter;
                }
+     if((flags[0][0] =="x" || flags[0][0] =="o") && (flags[0][1]=="x" || flags[0][1]=="o")
+                  && (flags[0][2]=="x" || flags[0][2]=="o") && (flags[1][0]=="x" || flags[1][0]=="o")
+                  && (flags[1][1]=="x" || flags[1][1]=="o") && (flags[1][2]=="x" || flags[1][2]=="o")
+                  && (flags[2][0]=="x" || flags[2][0]=="o") && (flags[2][1]=="x" || flags[2][1]=="o")
+                  && (flags[2][2]=="x" || flags[2][2]=="o")){
+                     // document.getElementById("rel").innerHTML="match draw";
+                     draw_counter++;
+                     draw.innerHTML=draw_counter;
+                     gameEnd = true;
+                     draw.style.color="white"
+                     
+                  }
 
 }
+
+
 btn.onclick = function(){
-   if(flags[i][j] == null){ draw.innerHTML = draw_counter;}
-}
+   
+   choice = 1
+   btn()
+   gameEnd = true
+   document.getElementById("rel").innerHTML="match draw";
+   for(let i=0;i<8;i++){
+      for(let j=0;j<8;j++){
+         if(flags[i][j]=="x" || flags[i][j]=="o"){
+            if(ply=="x" || ply=="o"){
+               buttons[i][j].innerHTML=""
+               buttons[i][j].classList.remove("x")
+               buttons[i][j].classList.remove("o")
+               flags[i][j]=null
+            }
+         }
+      }
+   }
+     }
